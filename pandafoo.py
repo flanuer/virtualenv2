40554b6a44988faee0b9538eaefa15620de965c1
Scott Phillpott <scott@phillpott.com>
2019-12-14 08:19:10 -0500

    Panda Works

--- pandafoo.py
+++ pandafoo.py
@@ -3,7 +3,11 @@
 import pandas as pd 
 import numpy as np 
 
-data = {
-    'apples': [3, 2, 0, 1],
-    'oranges': [0, 3, 7, 2]
-}
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+       "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+       "population": [200.4, 143.5, 1252, 1357, 52.98] }
+
+import pandas as pd
+brics = pd.DataFrame(dict)
+print(brics)
