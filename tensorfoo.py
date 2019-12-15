4ca96856f1f774034956d8a1141d171f0ad0ea6c
Scott Phillpott <scott@phillpott.com>
2019-12-15 08:07:56 -0500

    App.py breaks at Flask?  Don't know why

--- tensorfoo.py
+++ tensorfoo.py
@@ -1,1 +1,5 @@
-#learning tensorflow
+#learning tensorflow as part of the pattern recognition for Red Tide Research?
+
+# Alternatives to TensorFlow incluce PyTorch, CNTK, and NEXT
+
+# Perhaps start with PyTorch and develop models up to TensorFLow?
